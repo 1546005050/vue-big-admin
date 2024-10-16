be34349d35b489f77f2072605fe85ba13694db0e
芥尘 <78722951@qq.com>
2024-10-17 01:07:16 +0800

    拦截器，路由配置，登录页静态页的完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// createRouter用于创建实例
+// 配置history 模式
+// 1.hisyory模式：createWebHistory 地址栏不带#
+// 2.hash模式：createWebHashHistory地址栏带#号
 
+// vite中的环境变量import.meta.env.BASE_URL 就是bite.config.js中的base配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+    {path:'/login',component:()=>import('./views/login/LoginPage.vue'),
+      
+    },//登录页
+    {path:'/',component:()=>import('./views/layout/LayoutContainer.vue'),
+      redirect:'/article/manage',
+      children:[
+        {path:'/article/manage', component:()=>import('./views/article/ArticleChannel.vue')},
+        {path:'/article/channel', component:()=>import('./views/article/ArticleChannel.vue')},
+        {path:'/views/user/vatar', component:()=>import('./views/user/UserAvatar.vue')},
+        {path:'/views/user/password', component:()=>import('./views/user/UserPassword.vue')},
+        {path:'/views/user/profile', component:()=>import('./views/user/UserProfile.vue')},
+  
+      ]
+    }//架子
+  ],
 })
 
 export default router
